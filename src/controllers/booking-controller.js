539977c5fb8b5099dc833f192cce1a72a4767648
Sahil Patil <sahilpatil32267@gmail.com>
2025-06-26 19:36:40 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,29 +1,42 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../services/index");
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      err: {},
-      message: "Successfully completed booking",
-    });
-  } catch (error) {
-    return res
-      .status(error.StatusCodes || StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({
-        data: {},
-        success: false,
-        message: error.message,
-        err: error.explanation,
-      });
-  }
-};
+class BookingController {
+  constructor() {}
 
-module.exports = {
-  create,
-};
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "Success" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    return res.status(200).json({
+      message: "Successfully published the event ",
+    });
+  }
+
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        err: {},
+        message: "Successfully completed booking",
+      });
+    } catch (error) {
+      return res
+        .status(error.StatusCodes || StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          data: {},
+          success: false,
+          message: error.message,
+          err: error.explanation,
+        });
+    }
+  }
+}
+
+module.exports = BookingController;
