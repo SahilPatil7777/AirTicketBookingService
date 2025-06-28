6f763113951aed207185e41c451bb0bb3b4439b8
Sahil Patil <sahilpatil32267@gmail.com>
2025-06-28 09:54:56 +0530

    Added Payload in booking-Controller

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -10,8 +10,16 @@ class BookingController {
 
   async sendMessageToQueue(req, res) {
     const channel = await createChannel();
-    const data = { message: "Success" };
-    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    const payload = {
+      data: {
+        subject: "This is notification for queue",
+        content: "Some queue will subscribe this",
+        recepientEmail: "sahilpatil32267@gmail.com",
+        notificationTime: "2023-10-30T12:00:00",
+      },
+      service: "CREATE_TICKET",
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
     return res.status(200).json({
       message: "Successfully published the event ",
     });
